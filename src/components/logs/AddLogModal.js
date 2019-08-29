d1aa361883c15414e584d699915c897b8a7e411b
Fabricio Polo <fabricio.polo@gmail.com>
2019-08-29 11:11:13 -0300

    add botões e modal para add log

---
+++ src/components/logs/AddLogModal.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+const AddLogModal = () => {
+  const [message, setMessage] = useState("");
+  const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState("");
+
+  const onSubmit = () => {
+    if (message === "" || tech === "") {
+      M.toast({ html: "Por favor, complete a mensagem e/ou técnico." });
+    } else {
+      console.log(message, tech, attention);
+    }
+  };
+
+  return (
+    <div id='add-log-modal' className='modal' style={modalStyle}>
+      <div className='modal-content'>
+        <h4>Adicionar Log</h4>
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              name='message'
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+            />
+            <label htmlFor='message' className='active'>
+              Mensagem
+            </label>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='input-field'>
+            <select
+              name='tech'
+              value={tech}
+              className='browser-default'
+              onChange={e => setTech(e.target.value)}
+            >
+              <option value='' disabled>
+                Selecione um técnico
+              </option>
+              <option value='John Doe'>John Doe</option>
+              <option value='Sam Smith'>Sam Smith</option>
+              <option value='Sara Wilson'>Sara Wilson</option>
+            </select>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='input-field'>
+            <p>
+              <label>
+                <input
+                  type='checkbox'
+                  className='filled-in'
+                  checked={attention}
+                  value={attention}
+                  onChange={e => setAttention(!attention)}
+                />
+                <span>Requer Maior Atenção</span>
+              </label>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='modal-footer'>
+        <a
+          href='#!'
+          onClick={onSubmit}
+          className='modal-close waves-effect blue waves-light btn'
+        >
+          Cadastrar
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const modalStyle = {
+  width: "75%",
+  height: "75%"
+};
+
+export default AddLogModal;
