80e89d8acaef165e5e2713684ef65985ba4ff5cc
Fabricio Polo <fabricio.polo@gmail.com>
2019-08-30 10:27:54 -0300

    adicionado add log function

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
 
 //set loading = true
 export const setLoading = () => {
@@ -17,6 +17,32 @@ export const getLogs = () => async dispatch => {
 
     dispatch({
       type: GET_LOGS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    });
+  }
+};
+
+//adiciona novo log
+export const addLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const response = await fetch("/logs", {
+      method: "POST",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await response.json();
+
+    dispatch({
+      type: ADD_LOG,
       payload: data
     });
   } catch (error) {
