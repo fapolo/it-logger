2a946f4d8a694cebf52cdf4cedf6646d1d089eb8
Fabricio Polo <fabricio.polo@gmail.com>
2019-09-20 18:16:37 -0300

    add exclusao de tecnico

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -58,3 +58,24 @@ export const addTech = tech => async dispatch => {
     });
   }
 };
+
+//remove técnico do DB
+export const deleteTech = id => async dispatch => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: "DELETE"
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText
+    });
+  }
+};
